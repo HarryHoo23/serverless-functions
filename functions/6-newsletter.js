755c8435f8829ef74a70360c2937a33f107b59c2
Harry Hu <hoozh1123@gmail.com>
2022-05-04 14:18:29 +1000

    add email.js functions.

---
+++ functions/6-newsletter.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const axios = require('axios');
+const url = 'https://api.buttondown.email/v1/subscribers';
+
+exports.handler = async (event, context) => {
+    
+    const method = event.httpMethod;
+    if (method !== "POST") {
+        return {
+            statusCode: 405,
+            body: "Only POST Request Allowed"
+        }
+    }
+
+    const { email } = JSON.parse(event.body);
+    if (!email) {
+        return {
+            statusCode: 400,
+            body: "Please provide a valid email address!"
+        }
+    }
+
+    try {
+        const data = await axios.post(url, { email }, {
+            headers: {
+                Authorization: `Token ${process.env.EMAIL_API_KEY}`
+            }
+        });
+        console.log(data);
+        return {
+            statusCode: 201,
+            body: 'Success'
+        }
+    } catch (error) {
+         return {
+            statusCode: 400,
+            body:JSON.stringify(error.response.data)
+        }
+    }
+}
